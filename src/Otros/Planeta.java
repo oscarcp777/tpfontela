2e257ec950f5842c4be4a3ff0fee18f5c2723109
sivori.daniel <sivori.daniel@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2008-10-05 03:41:50 +0000

    se agregaron las naves al planeta

--- src/Otros/Planeta.java
+++ src/Otros/Planeta.java
@@ -2,4 +2,18 @@ package Otros;
 
 public class Planeta {
 
-}
+	private Nave naveEntrada;
+	public Nave getNaveEntrada() {
+		return naveEntrada;
+	}
+	public void setNaveEntrada(Nave naveEntrada) {
+		this.naveEntrada = naveEntrada;
+	}
+	public Nave getNaveEscape() {
+		return naveEscape;
+	}
+	public void setNaveEscape(Nave naveEscape) {
+		this.naveEscape = naveEscape;
+	}
+	private Nave naveEscape;
+} 
