ad0475c9614fb03ec5d859ff28cc8e82e126a1fb
cechechu.9 <cechechu.9@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2008-10-06 04:11:47 +0000

    falta ver como paso la posicion correcta en poogling.asignarPosicion([][]) y tmb comparar con el terreno

--- src/TipoHabilidades/Teletransportacion.java
+++ src/TipoHabilidades/Teletransportacion.java
@@ -1,13 +1,21 @@
 package TipoHabilidades;
 
+import Otros.Poogling;
 import TipoTerrenos.Terreno;
 
 public class Teletransportacion extends Habilidad{
-
-	@Override
-	public void destruirTerreno(Terreno terreno) {
-		// TODO Auto-generated method stub
 		
-	}
-
+		private Poogling poogling;
+		private Terreno terreno;
+		
+		public Teletransportacion(Poogling poogling,Terreno terreno){
+			this.poogling=poogling;
+			this.terreno=terreno;
+		}
+		
+		public void Teletransportar(){
+			
+			this.poogling=poogling.asignarPosicion([(posicion.getPosicionY)+20][posicion.getPosicionX]);
+		}
+		
 }
