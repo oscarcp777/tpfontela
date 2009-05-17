b1d650d3e8d4ee9cfad7d5bc29f7b45395a1b25f
caceres.oscar7 <caceres.oscar7@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-05-17 20:11:13 +0000

    Subo la primera versiooon del tp3

---
+++ TPTaller/TP3/src/Segmento.cpp
@@ -0,0 +1,31 @@
+#include "Segmento.h"
+#include <string>
+#include <iostream>
+#include "Escenario.h"
+
+Segmento::Segmento(std::string id,Posicion *inicio, Posicion *fin){
+		this->id = id;
+		this->inicio = inicio;
+		this->fin = fin;
+
+}
+Segmento::~Segmento(){
+	delete this->inicio;
+	delete this->fin;
+    
+}
+int Segmento::dibujar(SDL_Surface *screen){
+	this->color = getColorLinea()->getColor();
+
+	this->dibujarLinea(color,screen ,this->getInicio()->getX(),  this->getInicio()->getY(), this->getFin()->getX(), this->getFin()->getY());
+
+	return 0;
+}
+
+Posicion* Segmento::getInicio(){
+	return this->inicio;
+}
+
+Posicion* Segmento::getFin(){
+	return this->fin;
+}
