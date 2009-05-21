684c958704cbee1006fcd7a0e60e5f4797e806c1
santiagodonikian@gmail.com <santiagodonikian@gmail.com@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-05-21 21:44:11 +0000

    - AUN NO ANDA Y YA ME TOY CALENTANDO TUVE TODO EL DIA Y NO PUEDOOOO

---
+++ TPTaller/TP3/src/Utilidades.h
@@ -0,0 +1,49 @@
+#define TAM_MSJ 10000                 //Tamaño maximo del mensaje a enviar
+#define PRIMER_ENVIO 15   
+#define ARCH_CONF "config.txt"    //Nombre archivo configuración parser
+#define ARCH_LOG  "log.txt"     //Nombre archivo de log del parser
+#define TAM_NOMBRE_ARCH 30
+
+
+#define RES_OK 0
+#define RES_ERROR -1
+#define RES_QUIT -2
+#define RES_THREAD -3
+
+/*****************************************************************/
+/* recibir: Funcion que ejecuta en un hilo encargada de recibir    */
+/*          los mensajes que le son enviados y mostrarlos          */
+/*                                                                 */
+/*        conexion:  apunta a la  conexion.                        */
+/*        DWORD: devuelve el id del hilo que la esta ejecutando	   */
+/*****************************************************************/    
+DWORD WINAPI recibir(CONEXION *conexion);
+/*****************************************************************/
+/*****************************************************************/
+/* recibir: Funcion que ejecuta en un hilo encargada de enviar     */
+/*          los mensajes que le son enviados y mostrarlos          */
+/*        conexion:  apunta a la  conexion.                        */
+/*        DWORD: devuelve el id del hilo que la esta ejecutando	   */	
+/*****************************************************************/    
+DWORD WINAPI enviar(CONEXION *conexion);
+/*****************************************************************/
+/* reconectarSockets: funcion que acepta cuando  un cliente se    */
+/*                    conecta  cuando el cliente el que estaba    */
+/*                    conectado se desconecto                     */
+/*              devuelve RES_OK si alguien se conectó y pConexion */
+/*              apunta a la nueva conexion.                       */
+/*		  De lo contrario devuelve un codigo de error y           */
+/*             pConexion permanece inalterado.		              */	
+/*****************************************************************/    
+int reconectarSockets(CONEXION *pConexion);
+/*****************************************************************/
+/* ingresoMensaje: Espera que se ingrese un mensaje para ser enviado*/
+/*                 pmsj: mensaje ingresado  
+/*                 leyenda :mensaje para el que escribe   		    */	
+/*****************************************************************/    
+void ingresoMensaje(char *pmsj,char* leyenda);
+/*****************************************************************/
+/* deStringATipoDato: recibe un cadena de char y la devuelve      */ 
+/*                   como un tipò de dato definido anteriomente		      */	
+/*****************************************************************/    
+enum tr_tipo_dato deStringATipoDato(char* cadena);
