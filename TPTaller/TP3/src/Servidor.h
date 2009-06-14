6d8db96d7bedcc00761de4e6b0ff8f5b6d1051d6
santiagodonikian@gmail.com <santiagodonikian@gmail.com@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-06-14 18:57:26 +0000

    - no usa mas cSender

--- TPTaller/TP3/src/Servidor.h
+++ TPTaller/TP3/src/Servidor.h
@@ -3,7 +3,6 @@
 #include "Juego.h"
 #include "Thread.h"
 #include "Socket.h"
-#include "cSender.h"
 #include <iostream>
 #include <fstream>
 
@@ -21,5 +20,5 @@ class Servidor : public Thread
 		unsigned int participantesMax;
 		int puertoConexion;
 		Juego* juegoNuevo;
-		cSender sender;
+		
 };
