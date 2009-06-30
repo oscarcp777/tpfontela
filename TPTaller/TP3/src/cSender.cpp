62c687407dfe7b1ee1699baea634be1922700aff
santiagodonikian@gmail.com <santiagodonikian@gmail.com@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-06-30 16:15:40 +0000

    - posible fix, sino funca deshacer commit

--- TPTaller/TP3/src/cSender.cpp
+++ TPTaller/TP3/src/cSender.cpp
@@ -50,7 +50,17 @@ int cSender::process(void* args)
 		std::string moverArriba, moverAbajo;
 		std::string noMover = "NO MOVER\n";
 		std::string soltarTejo = "SOLTAR_TEJO\n";
+		char _moverArriba[40];
+		char *_pmoverArriba = _moverArriba;
+		char _noMover[40];
+		char *_pnoMover = _noMover;
+		char _moverAbajo[40];
+		char *_pmoverAbajo = _moverAbajo;
+		char _soltarTejo[40];
+		char *_psoltarTejo = _soltarTejo;
 		
+		_pnoMover = (char*)noMover.c_str();
+		_psoltarTejo = (char*)soltarTejo.c_str();
 
 		while (escenario->getPadJugador()==NULL){
 			sleep(3000);
@@ -60,12 +70,17 @@ int cSender::process(void* args)
 	
 		if(escenario->getNumJugador() == 1){
 			moverArriba = "PAD1 ARRIBA\n";
+			_pmoverArriba = (char*)moverArriba.c_str();
 			moverAbajo =  "PAD1 ABAJO\n";
+			_pmoverAbajo = (char*)moverAbajo.c_str();
+			
 		}
 		else if(escenario->getNumJugador() == 2){
 			
 			moverArriba =  "PAD2 ARRIBA\n";
+			_pmoverArriba = (char*)moverArriba.c_str();
 			moverAbajo = "PAD2 ABAJO\n";
+			_pmoverAbajo = (char*)moverAbajo.c_str();
 		}
 				
 		
@@ -73,20 +88,20 @@ int cSender::process(void* args)
 			Sleep(20);
 		
 			if(pad->getMoverArriba()){
-				sock->send((char*)moverArriba.data());
+				sock->send(_pmoverArriba);
 				pad->setMoverArriba(false);
 			}
 			else if(pad->getMoverAbajo()){
-				sock->send((char*)moverAbajo.data());
+				sock->send(_pmoverAbajo);
 				pad->setMoverAbajo(false);
 			}
 			else if(pad->getSoltarTejo()){
-				sock->send((char*)soltarTejo.data());
+				sock->send(_psoltarTejo);
 				escenario->getPadJugador()->setSoltarTejo(false);
 			
 			}
 			else if(!pad->getMoverArriba() && !pad->getMoverAbajo()){
-				sock->send((char*)noMover.data());
+				sock->send(_pnoMover);
 				
 			}
 			
