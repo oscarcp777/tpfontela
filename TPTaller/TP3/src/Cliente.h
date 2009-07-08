da812df71a0fdbf17118f603db9bf3b6254c0288
santiagodonikian@gmail.com <santiagodonikian@gmail.com@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-07-08 19:51:16 +0000

    - commit seguro. saco comentarios y variables no usadas

--- TPTaller/TP3/src/Cliente.h
+++ TPTaller/TP3/src/Cliente.h
@@ -19,21 +19,12 @@ public:
         Cliente();
         // Se conecta al servidor y lanza los threads correspondientes
         void start(char*  host, int port);
-        // Envia el mensaje al "servidor proxy" (lo encola para enviar)
-        void send(std::string msg);
         // Obtiene un mensaje del "servidor proxy" (lo desencola de los recibidos)
 		std::string get();
-        // Chequea si hay instrucciones encoladas
-        bool haveInstructions();
         // Chequea el estado del servidor (conectado, no conectado, etc)
         int getStatus();
         // Setea el estado del servidor
         void setStatus(int status);
-        // Obtiene el porcentaje descargado del fichero (en caso que se este bajando uno)
-        float getDownloadedPercent();
-        // Resetea el porcentaje descargado
-        void clearDownloaded();
-        // Detiene la ejecucion del "server proxy" (cierra el socket y joinea los threads)
         void stop();
 		void loading(Socket* s);
 		~Cliente();
