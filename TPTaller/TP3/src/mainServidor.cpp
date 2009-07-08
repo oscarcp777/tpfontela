6af48dee7ba24dbb255871b82e108ec5d25771bc
santiagodonikian@gmail.com <santiagodonikian@gmail.com@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-07-08 19:47:43 +0000

    - commit seguro. saco comentarios y variables no usadas

--- TPTaller/TP3/src/mainServidor.cpp
+++ TPTaller/TP3/src/mainServidor.cpp
@@ -10,12 +10,7 @@ using namespace std;
 int main(int argc, char* argv[]){
 	int error = 0;
 
-	char leyenda[30];
-	char* pLeyenda = leyenda;
-	char puerto[50];
-	pLeyenda = "PUERTO DE CONEXION (EJ: 2121)";
 
-	//ingresoMensaje(puerto,pLeyenda);
 
 	Servidor* s = new Servidor(1050, 2);
 	s->start(NULL);
