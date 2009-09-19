befa06043d7fa3da686787b4784331f069a2a5f4
rdubini <rdubini@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-09-19 21:29:50 +0000

    -main

--- TPDatos/src/TPDatos.cpp
+++ TPDatos/src/TPDatos.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!NOS VAN A ROMPER EL ORTO EN DATOS LALALA!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
