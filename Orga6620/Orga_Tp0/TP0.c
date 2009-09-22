41db8967c8463cac2a76b9c76b8e0fc3ee52a857
santiagodonikian <santiagodonikian@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-09-22 13:51:59 +0000

    - printf fixed, listBytes bug fixed

--- Orga6620/Orga_Tp0/TP0.c
+++ Orga6620/Orga_Tp0/TP0.c
@@ -24,14 +24,14 @@ static struct option long_options[] ={
 typedef struct options_flags{
 	int flag_delimiter;
 	char p_delimiter[10];
-	
+
 	int flag_bytes;
 	char p_bytes[50];
-	
+
     int flag_field;
 	char p_field[50];
-	
-    int flag_ignore;	
+
+    int flag_ignore;
     //Si hay un archivo de entrada ifile = 1
 	int flag_ifile;
 }Oflags;
@@ -62,84 +62,84 @@ void getField(char* line, int nField, char delimiter, char* field);
 int main(int argc, char** argv){
 	char* ofname;
 	//char field[80];
-    char cadena[80]	;
+	char cadena[80]	;
 	int i = 0;
 	//int k=0;
 	FILE* inputFile;
 
 	char delimiterDefault[10]="\t";
 	memcpy(oflags.p_delimiter, delimiterDefault, 10);
-	
-	getOptions(argc,argv,&ofname);
-		
-    if (validateCommand() != EXIT_SUCCESS)
-       return -1;	
 
-    if(oflags.flag_ifile == 0){
-       	fgets(cadena, 80, stdin);   
-	    	    
-    if(oflags.flag_field != 0){
-        if(validateRange(oflags.p_field) == NULL){
-               help();         
-               return -1;
-        }else
-               listFields(cadena); 
-    }
-    else if(oflags.flag_bytes == 1){                             
-             if(validateRange(oflags.p_bytes) == NULL){
-                  help();         
-                  return -1;
-             }else         
-                  listBytes(cadena);	    	
-         }    
-	    	    
-    }
-    else{
-         for(i = 0; i < 10; i++){
-    			if(posicionesArchivos[i] != -1){
-					filename = argv[posicionesArchivos[i]];
-  		    	    inputFile = fopen(filename, "r");
-    
-    				if(inputFile == NULL){
-    					fprintf(stderr, "No se puede abrir el archivo!! %s!\n" ,filename);
-    					return -1;
-    				}
-    				while(fgets(cadena, 80, inputFile) != NULL) { 
-                         if(oflags.flag_field != 0){
-                            if(validateRange(oflags.p_field) == NULL){
-                                 help();         
-                                 return -1;
-                            }else
-                                 listFields(cadena); 
-                         }   
-                         else if(oflags.flag_bytes == 1){                             
-                                  if(validateRange(oflags.p_bytes) == NULL){
-                                       help();         
-                                       return -1;
-                                  }else         
-                                       listBytes(cadena);	
-                           }
-                     }
-                     fclose(inputFile);
-    			}
-	      }
-       }
-            
+	getOptions(argc,argv,&ofname);
+
+	if (validateCommand() != EXIT_SUCCESS)
+		return -1;
+
+	if(oflags.flag_ifile == 0){
+		fgets(cadena, 80, stdin);
+
+		if(oflags.flag_field != 0){
+			if(validateRange(oflags.p_field) == NULL){
+				help();
+				return -1;
+			}else
+				listFields(cadena);
+		}
+		else if(oflags.flag_bytes == 1){
+			if(validateRange(oflags.p_bytes) == NULL){
+				help();
+				return -1;
+			}else
+				listBytes(cadena);
+		}
+
+	}
+	else{
+		for(i = 0; i < 10; i++){
+			if(posicionesArchivos[i] != -1){
+				filename = argv[posicionesArchivos[i]];
+				inputFile = fopen(filename, "r");
+
+				if(inputFile == NULL){
+					fprintf(stderr, "No se puede abrir el archivo!! %s!\n" ,filename);
+					return -1;
+				}
+				while(fgets(cadena, 80, inputFile) != NULL) {
+					if(oflags.flag_field != 0){
+						if(validateRange(oflags.p_field) == NULL){
+							help();
+							return -1;
+						}else
+							listFields(cadena);
+					}
+					else if(oflags.flag_bytes == 1){
+						if(validateRange(oflags.p_bytes) == NULL){
+							help();
+							return -1;
+						}else
+							listBytes(cadena);
+					}
+				}
+				fclose(inputFile);
+			}
+		}
+	}
+
 	return EXIT_SUCCESS;
 }
 
 int validateCommand(){
-    
+
     	if(oflags.flag_delimiter == 1 && oflags.flag_field == 0 ){
-                  help();                
+                  help();
                   return -1;
-            } 
-             
+            }
+
         if(oflags.flag_bytes == 1 && oflags.flag_field == 1 ){
-                  help();                
+                  help();
                   return -1;
-        } 
-        
+        }
+
         return EXIT_SUCCESS;
 }
 
@@ -168,9 +168,9 @@ int getOptions(int argc, char** argv, char** ofname){
 	int c;
 	char *aux;
     opterr = 0;
-   
+
     while((c = getopt_long(argc, argv, modifiers,long_options, &option_index)) != EOF){
-    	
+
         switch (c){
         	case 'V':
         		getVersion();
@@ -184,7 +184,7 @@ int getOptions(int argc, char** argv, char** ofname){
                  if (optarg != NULL)
                      memcpy(aux,optarg,sizeof(char)*10);
                  else
-                     help(); 
+                     help();
         	break;
         	case 'b':
                  oflags.flag_bytes = 1;
@@ -192,7 +192,7 @@ int getOptions(int argc, char** argv, char** ofname){
         		 if (optarg != NULL)
                     memcpy(aux,optarg,sizeof(char)*50);
                  else
-                     help();  
+                     help();
         	break;
         	case 'f':
                  oflags.flag_field = 1;
@@ -200,14 +200,14 @@ int getOptions(int argc, char** argv, char** ofname){
                  if (optarg != NULL)
                      memcpy(aux,optarg,sizeof(char)*50);
                  else
-                    help();      
+                    help();
             break;
         	default:
         		help();
         		abort();
     	}
 
-    }    
+    }
     if (optind < argc){
     	int j = 0;
     	//printf ("Nombres Archivos: \n\t");
@@ -219,7 +219,7 @@ int getOptions(int argc, char** argv, char** ofname){
     	}
     	numParameters = j;
     }
-    
+
     return EXIT_SUCCESS;
 }
 
@@ -227,58 +227,70 @@ int listBytes(char* string){
     int i = 0;
     int j = 0;
     char* aux;
-    char field[50]; 
+    char field[50];
     memset(field,0,80);
     aux = string;
-    
     while (rango[i]!= -5){
           if (rango[i+1] == -1){
              rango[i+1] = -5;
-             while (j<rango[i]){
-                   j++;
-                   aux++;
+             //si el menor campo pedido es mayor que la cantidad de bytes de la linea no devuelve nada
+             if (rango[i] < strlen(string)){
+            	 while (j<rango[i]){
+            		 j++;
+            		 aux++;
+            	 }
+            	 aux--;
+            	 strcpy(field,aux);
+            	 printf("%s", field);
              }
-             aux--;
-             strcpy(field,aux);
-             printf("%s\n", field);  
           }else
                if (rango[i+1] == -2){
-                  rango[i+1] = -5;            
+                  rango[i+1] = -5;
                   while (j<rango[i]){
                    j++;
                    aux++;
                   }
-                  aux--;
-                   strncpy(field,string,aux-string);
-                   printf("%s\n", field);
+                  //si el mayor campo pedido es mayor que la cantidad de bytes de la linea devuelve
+                  //hasta el final de la linea
+                  if (rango[i] < strlen(string)){
+                	  strncpy(field,string,aux-string);
+                	  printf("%s\n", field);
+                  }else{
+                	  strcpy(field,aux);
+                	  printf("%s", string);
+                  }
+
                }else{
-                        while (j<rango[i]){
-                              j++;
-                              aux++;
-                        }
-                        aux--;
-                        printf("%c\n", *aux); 
-                    }
-          j = 0; 
-          aux = string;         
+            	   //si el campo pedido es mayor que la cantidad de bytes de la linea no devuelve nada
+            	   if (rango[i] < strlen(string)){
+            		   while (j<rango[i]){
+            			   j++;
+            			   aux++;
+            		   }
+            		   aux--;
+            		   printf("%c\n", *aux);
+            	   }
+               }
+          j = 0;
+          aux = string;
           i++;
     }
-   
-    return EXIT_SUCCESS;    
+
+    return EXIT_SUCCESS;
 }
 int listFields(char* string){
-    
+
     int i = 0;
     int min = 0;
     int max = 0;
     int tFields;
     int aux;
-    char field[80]; 
+    char field[80];
     memset(field,0,80);
     tFields = totalFields(string,*(oflags.p_delimiter));
     if (tFields == 1)
        return EXIT_SUCCESS;
-    
+
     while (rango[i]!= -5){
           if (rango[i+1] == -2){
              aux = rango[i];
@@ -296,48 +308,48 @@ int listFields(char* string){
     if (min)
        for (i=1;i<=aux;i++){
             memset(field,0,80);
-            getField(string, i,*(oflags.p_delimiter), field);     
-       } 
+            getField(string, i,*(oflags.p_delimiter), field);
+       }
     if (max){
        for (i=aux;i<=tFields;i++){
             memset(field,0,80);
-            getField(string, i,*(oflags.p_delimiter), field);     
-       }   
-    } 
-    return EXIT_SUCCESS;     
+            getField(string, i,*(oflags.p_delimiter), field);
+       }
+    }
+    return EXIT_SUCCESS;
 }
 int totalFields(char* line,char c) {
         char* lAux;
         int total = 0;
         lAux = line;
-        
+
         while (lAux!=NULL){
-              if ((lAux=strchr(++lAux,c))!= NULL) 
-              total++; 
+              if ((lAux=strchr(++lAux,c))!= NULL)
+              total++;
         }
         total++;
         return total;
 }
 
 void getField(char* line, int nField, char delimiter, char* field){
- 
+
        // char* lAux;
         char c = delimiter;
         int tFields = 0;
         int i, size;
         char *fBegin , *fEnd;
-        
+
         fBegin = fEnd = line;
-        
-        tFields = totalFields(line, delimiter);       
-           
+
+        tFields = totalFields(line, delimiter);
+
         if (tFields == 1)
           strcpy(field,fBegin);
-        else{  
+        else{
            for(i=0;i<nField;++i){
               fBegin=fEnd;
-              fEnd=strchr(fEnd,c); 
-              if (fEnd!=NULL) 
+              fEnd=strchr(fEnd,c);
+              if (fEnd!=NULL)
                  ++fEnd;
            }
            if (nField==tFields)
@@ -348,8 +360,10 @@ void getField(char* line, int nField, char delimiter, char* field){
              strncpy(field,fBegin,size);
            }
         }
-        printf("%s\n", field); 
-      
+        printf("%s", field);
+        if (nField != tFields)
+        	printf("%c",delimiter);
+
 }
 
 
@@ -364,31 +378,31 @@ int numDigitos( int numero ){
 
 
 int* validateRange(char* datos){
-      
+
       char* pDatos;
       int i = 0;
       int aux1;
       int aux2;
-      int iInicial;   
+      int iInicial;
       int diferencia;
       int digitos;
-      
+
     if(strpbrk(datos,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.:;!��?_+*[]{}����#$%&^/=`~") != NULL){
-		 printf("tiene caracteres incorrectos \n"); 
-        return NULL;	
+		 printf("tiene caracteres incorrectos \n");
+        return NULL;
 	}
 	else{
          pDatos = datos;
-         
+
          while(*pDatos != '\0'){
-               
+
                if(i==0){
                //si es el primer caracter leido
                      if(*pDatos == '-'){
                                 //si es un guion, ej -5 pongo en el vector 5,-2
-                                rango[i+1]= -2; 
+                                rango[i+1]= -2;
                                 pDatos++;
-                                rango[i]= atoi(pDatos);  
+                                rango[i]= atoi(pDatos);
                                 digitos = numDigitos(rango[i]);
                                 while(digitos){
                                   pDatos++;
@@ -398,17 +412,17 @@ int* validateRange(char* datos){
                      }
                      else if(*pDatos == ','){
                           //no puede empezar con una "," ERROR
-                          return NULL;     
-                     }                     
+                          return NULL;
+                     }
                      else{
-                          //si es un numero                          
+                          //si es un numero
                           rango[i] = atoi(pDatos);
                           digitos = numDigitos(rango[i]);
                           while(digitos){
                               pDatos++;
                               digitos--;
-                          } 
-                          i++;          
+                          }
+                          i++;
                      }
                }
                else{
@@ -416,21 +430,21 @@ int* validateRange(char* datos){
                     if(*pDatos == '-'){
                                pDatos++;
                                 if(*pDatos == '\0'){
-                                   //si es un guion y le sigue un \0, ej 5- pongo 5,-1 
-                                   rango[i]= -1;    
+                                   //si es un guion y le sigue un \0, ej 5- pongo 5,-1
+                                   rango[i]= -1;
                                    i++;
                                 }
                                 else if(*pDatos == ','){
                                      //no puede empezar con una "," ERROR
-                                     return NULL;     
+                                     return NULL;
                                 }
                                 else{
-                                   //si es un guion y le sigue un numero, ej 2-5 pongo 2,3,4,5 pero si el primero es mayor al segundo es un ERROR  (ej 5-2) 
+                                   //si es un guion y le sigue un numero, ej 2-5 pongo 2,3,4,5 pero si el primero es mayor al segundo es un ERROR  (ej 5-2)
                                      aux1 = rango[i-1];// ultimo numero guardado
                                      aux2 = atoi(pDatos);//obtengo el siguiente despues del "-"
-                                     
+
                                      if(aux2<=aux1)//es un error(ej 5-2)
-                                           return NULL;  
+                                           return NULL;
                                      else{
                                         iInicial = i;
                                         diferencia = aux2-aux1;
@@ -443,15 +457,15 @@ int* validateRange(char* datos){
                                         digitos = numDigitos(aux1);
                                         while(digitos){
                                               pDatos++;
-                                              digitos--;                                                                                                                 
+                                              digitos--;
                                         }
-                                     }        
+                                     }
                                 }
                      }
-                     else if(*pDatos == ','){                          
+                     else if(*pDatos == ','){
                            pDatos++;
                            if( (*pDatos == ',') || (*pDatos == ' ') || (*pDatos == '-') )
-                               return NULL; 
+                               return NULL;
                            else{
                                 //si es un numero
                                 rango[i] = atoi(pDatos);
@@ -459,10 +473,10 @@ int* validateRange(char* datos){
                                 while(digitos){
                                      pDatos++;
                                      digitos--;
-                                } 
-                                i++;  
+                                }
+                                i++;
                            }
-                     }                     
+                     }
                      else{
                           //si es un numero
                           rango[i] = atoi(pDatos);
@@ -470,12 +484,12 @@ int* validateRange(char* datos){
                           while(digitos){
                                pDatos++;
                                digitos--;
-                          } 
-                          i++; 
+                          }
+                          i++;
                      }
-                    }               
+                    }
           }
 		 return rango;
 		}
-    
+
 }
