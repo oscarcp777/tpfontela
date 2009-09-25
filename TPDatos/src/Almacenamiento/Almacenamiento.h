d344287b0753b0471f00ae817301357cd7b266f7
caceres.oscar7 <caceres.oscar7@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-09-25 02:08:53 +0000

    Agregue algo de la funcionalidad de los patrones

--- TPDatos/src/Almacenamiento/Almacenamiento.h
+++ TPDatos/src/Almacenamiento/Almacenamiento.h
@@ -7,11 +7,16 @@
 
 #ifndef ALMACENAMIENTO_H_
 #define ALMACENAMIENTO_H_
-
+#include "../Composite/Compuesto.h"
 class Almacenamiento {
+private:
+	Compuesto* compuesto;
 public:
 	Almacenamiento();
 	virtual ~Almacenamiento();
+    Compuesto *getCompuesto();
+
+    void setCompuesto(Compuesto *compuesto);
 };
 
 #endif /* ALMACENAMIENTO_H_ */
--- TPDatos/src/Composite/Compuesto.h
+++ TPDatos/src/Composite/Compuesto.h
@@ -7,12 +7,18 @@
 
 #ifndef COMPUESTO_H_
 #define COMPUESTO_H_
+#include <list>
 #include "../Object/Object.h"
-
+#include "Componente.h"
 class Compuesto: public Object {
+private:
+	std::list<Componente*> listaDeComponetes;
 public:
 	Compuesto();
 	virtual ~Compuesto();
+	std::list<Componente*>::iterator iteratorListaDeComponetes();
+    int getCantidadDeElelmentos();
+
 };
 
 #endif /* COMPUESTO_H_ */
