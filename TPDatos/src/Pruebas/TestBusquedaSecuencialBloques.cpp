8cbfc3f681729447b3e4902d06528ee1c48581dc
rdubini <rdubini@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2009-10-17 19:37:50 +0000

    Separando pruebas

---
+++ TPDatos/src/Pruebas/TestBusquedaSecuencialBloques.cpp
@@ -0,0 +1,36 @@
+/*
+ * TestBusquedaSecuencial.cpp
+ *
+ *  Created on: 12/10/2009
+ *      Author: oscar
+ */
+#include <iostream>
+#include "../Object/Object.h"
+#include "../utils/Define.h"
+#include "../Modelo/Alumno.h"
+#include "../Composite/Componente.h"
+#include "../Factory/FabricaDeRecursosDeAlmacenamiento.h"
+#include "../Almacenamiento/RecursoDeAlmacenamiento.h"
+#include <vector>
+#include "../utils/StringUtils.h"
+using namespace std;
+
+int main() {
+
+	FabricaDeRecursosDeAlmacenamiento* fabricaDeRecursos=FabricaDeRecursosDeAlmacenamiento::obtenerInstancia();
+
+	RecursoDeAlmacenamiento* recurso=
+				fabricaDeRecursos->RecursoDeAlmacenamientoEnArchivo(ESTRATEGIA_ALMACENAMIENTO_BLOQUES,55);
+
+		recurso->leerArchivo("/home/richy/","Bloques");
+		Alumno* alumno = new Alumno();
+		recurso->busquedaSecuencial(alumno,"85440");
+		std::cout<<"resultados busqueda: "<<std::endl;
+		std::cout<<"nombre: "<<alumno->getNombre()<<std::endl;
+		std::cout<<"padron: "<<alumno->getPadron()<<std::endl;
+	delete recurso;
+	delete fabricaDeRecursos;
+	return 0;
+
+
+}
