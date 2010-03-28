c462810e0cc6bddbc438ae5496228e9f5c934112
rdubini <rdubini@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-03-28 21:57:58 +0000

    -Agregue la prueba del hash, falta el archivo y el buffer para poder probarla

---
+++ Datos2010/src.datos.hashing/testHash.cpp
@@ -0,0 +1,34 @@
+/*
+ * testHash.cpp
+ *
+ *  Created on: 28/03/2010
+ *      Author: richy
+ */
+#include "Directory.h"
+#include "Hash.h"
+#include <string>
+
+int main234(){
+	int result;
+	string keys[15] = {"osky" , "nelson" , "santy", "dani", "richy", "caceres", "ramos", "donikian", "sivori", "dubini","maja","paucartambo","datos","hash","kit"};
+
+
+	string nombre = "archivoHash";
+	Directory dir(4);
+	result = dir.create((char*)nombre.c_str());
+	if(result == 0){
+		std::cout<<"borre los archivos e intente nuevamente"<<std::endl;
+		return 0;
+	}
+
+	const int numKeys = 15;
+	for(int i=0; i<numKeys; i++){
+		std::cout<<keys[i]<<" "<<(void*)hash((char*)(keys[i]).c_str())<<" "<<(void*)makeAddress((char*)(keys[i]).c_str(),16)<<std::endl;
+		result = dir.insert((char*)(keys[i]).c_str(), 100+i);
+		if(result == 0)
+			std::cout<<" insert for "<<keys[i]<<" failed"<<std::endl;
+	}
+	dir.print();
+	return 1;
+
+}
