b5b693cf975355b502490d2aa459e5ee40574609
lomasfragildelalocura <lomasfragildelalocura@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-03-28 17:37:00 +0000

    Clase logger con algunos errores que se solucionaran a la brevedad.

--- Datos2010/src.datos.logger/Logger.cpp
+++ Datos2010/src.datos.logger/Logger.cpp
@@ -6,30 +6,88 @@
  */
 
 #include "Logger.h"
+#include "time.h"
 
-	Logger* Logger::logger = 0;
+Logger* Logger::logger = NULL;
 
-	static Logger* Logger::getInstacia(){
+Logger::Logger(){
 
-		if(logger==0){
-		/*
-		 * Aca instancio abro los archivos.
-		 */
-		}
+}
 
-		return this->logger;
+std::string Logger::getTimeSystem(){
+
+	time_t rawtime;
+	struct tm* timeinfo;
+	time ( &rawtime );
+	timeinfo = localtime (&rawtime);
+	return asctime(timeinfo);
+}
+
+void Logger::open(fstream file, string fileName){
+
+	file.open(fileName.c_str(), ios::in | ios::out);
+
+	if (!this->file.is_open()) {
+
+		this->file.clear();
+		this->file.open(fileName.c_str(), ios::out);
+		this->file.close();
+		this->file.open(fileName.c_str(), ios::in | ios::out);
 	}
-	/*
-	 * En los metodos que se encuentran a continuacion
-	 * tengo que escribir los archivos.
-	 */
-	void Logger::setInfo(std::string cadena);
-	void Logger::setDebug(std::string object, std::string cadena);
-	void Logger::setError(std::string object, std::string cadena);
-	/*
-	 * El logger se instancia al comienzo de la aplicacion
-	 * por lo tanto cuando esta se termine llamara al destructor
-	 * del logger automaticamente y es ahi donde se cierran los archivos
-	 * y liberan los recursos.
-	 */
-	Logger::~Logger();
+
+}
+
+void Logger::write(fstream file, string cadena){
+
+	if (file.is_open()){
+
+
+		file.seekg(0, ios::end);
+		file << cadena << endl;
+	}
+}
+
+Logger* Logger::getUnicaInstance(){
+
+	if(!Logger::logger){
+		/*
+		 * Aca instancio el singleton y abró los archivos.
+		 */
+		Logger::logger = new Logger();
+
+		this->open(this->fileInfo,PATH_INFO);
+		this->open(this->fileDebug,PATH_DEBUG);
+		this->open(this->fileError,PATH_ERROR);
+	}
+
+	return logger;
+}
+
+void Logger::setInfo(std::string cadena){
+
+	this->write(this->fileInfo,this->getTimeSystem() + this->cadena);
+}
+
+void Logger::setDebug(std::string object, std::string cadena){
+
+	this->write(this->fileDebug,this->getTimeSystem() + object + cadena);
+}
+
+void Logger::setError(std::string object, std::string cadena){
+
+	this->write(this->fileError,this->getTimeSystem() + object + cadena);
+}
+/*
+ * El logger se instancia al comienzo de la aplicacion
+ * por lo tanto cuando esta se termine llamara al destructor
+ * del logger automaticamente y es ahi donde se cierran los archivos
+ * y liberan los recursos.
+ */
+Logger::~Logger(){
+
+	//Cierra los archivos abiertos.
+	this->fileInfo.close();
+	this->fileDebug.close();
+	this->fileError.close();
+
+}
--- Datos2010/src.datos.logger/Logger.h
+++ Datos2010/src.datos.logger/Logger.h
@@ -8,29 +8,47 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
-#include "Object.h"
-#include "TextFile.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 class Logger {
 
 private:
 
 	static Logger* logger;
-	TextFile* fileInfo;
-	TextFile* fileDebug;
-	TextFile* fileError;
+	fstream fileInfo;
+	fstream fileDebug;
+	fstream fileError;
 	/*
 	 * Constructor privado, para que solo se instancie
 	 * mediante el metodo getInstacia.
 	 */
 	Logger();
+	/*
+	 * Apertura de archivos, si el archivo esta creado, lo abre.
+	 * Si no esta creado, lo crea y lo deja activo para su
+	 * utilizacion.
+	 */
+	void open(fstream file, string fileName);
+
+	/*
+	 * Escritura de los archivos, si el archivo no existe el logger
+	 * lo crea y lo deja y escribe. Si existe, simplemente escribe
+	 * sobre este.
+	 */
+	void write(fstream file,string cadena);
+	/*
+	 * Retorna la fecha y hora del sistema.
+	 */
+	std::string getTimeSystem();
 
 public:
 	/*
 	 * Se obtiene un puntero a la unica instancia del
 	 * logger.
 	 */
-	static Logger* getInstacia();
+	static Logger* getUnicaInstance();
 	/*
 	 * Escribe en el archivo de salida de informacion
 	 * la info seteada.
