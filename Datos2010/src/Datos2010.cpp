03c04f00e858d48bfb261967f71bc36ee613c41b
rdubini <rdubini@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-04-11 22:37:00 +0000

    -prueba hash

--- Datos2010/src/Datos2010.cpp
+++ Datos2010/src/Datos2010.cpp
@@ -16,17 +16,23 @@ using namespace std;
 int main() {
 	HashExtensible* hash = new HashExtensible();
 	hash->create("hash");
-	string cadena = "el hash ya esta, y el arbol???";
-//	string cadena2 = "Maja no jodas";
-	hash->insert(85440,(char*)cadena.c_str());
-//	hash->insert(55443,(char*)cadena2.c_str());
-//	hash->insert(854340,(char*)cadena.c_str());
-//	hash->insert(25220,(char*)cadena.c_str());
-//	hash->insert(81120,(char*)cadena.c_str());
-//	hash->insert(85499,(char*)cadena.c_str());
-//	hash->insert(75440,(char*)cadena.c_str());
+	string cadena = "Oscar Caceres";
+	string cadena2 = "Richard Dubini";
+	string cadena3 = "santy Donikian";
+//	string cadena4 = "Nelson Arequipa";
+//	string cadena5 = " Daniel Sivori";
+	hash->insert(86534,(char*)cadena.c_str());
+	hash->insert(85440,(char*)cadena2.c_str());
+	hash->insert(85789,(char*)cadena3.c_str());
+//	hash->insert(82289,(char*)cadena4.c_str());
+//	hash->insert(45739,(char*)cadena5.c_str());
+
 	hash->close();
 
+	hash->open("hash");
+	cout<<"data key: "<<hash->search(85789)<<endl;
+	hash->remove(86534);
+	hash->close();
 	return 0;
 
 }
