b66370def4496ffda1eb125d5d1dde6023fc82ab
caceres.oscar7 <caceres.oscar7@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-04-13 05:49:24 +0000

    vieja: Ya arregle el bug que tenias era que el vector de offset tenia un elemento de mas que era el cero que le asignabas en el constructor lo asigno despues el cero en el create dela tabla anda todo mono fijate las prubas que hice falta probar solo el eliminar
    a mi no me pone fruto como en la tuya fijate los archivos que comitie en la carpeta files
    
    IMPORTANTE :usa loas archivos que  te comitie que no tienen fruta

--- Datos2010/src/PruebaHash.cpp
+++ Datos2010/src/PruebaHash.cpp
@@ -10,39 +10,71 @@
 #include "../src.datos.storage/BinaryFile.h"
 #include "../hashExtensible/HashExtensible.h"
 using namespace std;
-
-int main(){
-//	HashExtensible* hash = new HashExtensible();
-//	hash->create("hash");
-//	string cadena = "Oscar Caceres";
-//	string cadena2 = "Richard Dubini";
-//	string cadena3 = "santy Donikian";
-//	string cadena4 = "Nelson Arekipa";
-//	string cadena5 = " Daniel Sivori";
-//	string cadena6 = " Maja perra";
-//	string cadena7 = "PAUCARTAMBO";
-//	string cadena8 = "River Plate";
-//	string cadena9 = "Facultad Ingenieria";
-//	hash->insert(86534,(char*)cadena.c_str());
-//	hash->insert(85440,(char*)cadena2.c_str());
-//	hash->insert(85789,(char*)cadena3.c_str());
-//	hash->insert(82289,(char*)cadena4.c_str());
-//	hash->insert(45739,(char*)cadena5.c_str());
-//	hash->insert(11739,(char*)cadena6.c_str());
-//	hash->insert(56739,(char*)cadena7.c_str());
-//	hash->insert(98339,(char*)cadena8.c_str());
-//	hash->insert(87345,(char*)cadena9.c_str());
-//	hash->print(NULL);
-//
-//	hash->close();
-//	cout<<"DESPUES DEL CLOSE "<<endl;
-//	delete hash;
-
+void createHashTable(){
 	HashExtensible* hash = new HashExtensible();
-	hash->open("hash");
-	cout<<"data encontrada : " <<hash->search(85440)<<endl;
+	hash->create("files/hash2");
+	string cadena = "Oscar Caceres Paredes";
+	string cadena2 = "Richard Marco Dubini";
+	string cadena3 = "santiago raul Donikian";
+	string cadena4 = "Nelson castro Arekipa";
+	string cadena5 = " Daniel pablo Sivori";
+	string cadena6 = "Diego armando maradona";
+	string cadena7 = "pedro jose ladron de guevarra ortiz de orue";
+	string cadena8 = "juan paez";
+	string cadena9 = "boca rey de copas el mas grande de todos los tiempos y mas alla";
+	hash->insert(86534,(char*)cadena.c_str());
+	hash->insert(85440,(char*)cadena2.c_str());
+	hash->insert(85789,(char*)cadena3.c_str());
+	hash->insert(82289,(char*)cadena4.c_str());
+	hash->insert(45739,(char*)cadena5.c_str());
+	hash->insert(11739,(char*)cadena6.c_str());
+	hash->insert(56739,(char*)cadena7.c_str());
+	hash->insert(87346,(char*)cadena8.c_str());
+	hash->insert(87345,(char*)cadena9.c_str());
+	hash->print(NULL);
+
+	hash->close();
+
+	delete hash;
+	cout<<"Borro todo bien "<<endl;
+}
+void openHash(){
+	HashExtensible* hash = new HashExtensible();
+	hash->open("files/hash2");
+	cout<<"data encontrada : " <<hash->search(87345)<<endl;
+//	cout<<"data encontrada : " <<hash->search(98339)<<endl;
+//	cout<<"data encontrada : " <<hash->search(56739)<<endl;
+//	cout<<"data encontrada : " <<hash->search(11739)<<endl;
+//	cout<<"data encontrada : " <<hash->search(45739)<<endl;
+//	cout<<"data encontrada : " <<hash->search(82289)<<endl;
+//	cout<<"data encontrada : " <<hash->search(85440)<<endl;
 	hash->close();
 	delete hash;
 	cout<<"FIN PRUEBA "<<endl;
+}
+int main(){
+
+	//createHashTable();
+	openHash();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
