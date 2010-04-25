3d687b2bfb8ac337994d7e1d55298c91a47f9bdb
santiagodonikian <santiagodonikian@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-04-25 22:31:03 +0000

    manejo bloques libres casi terminado

--- Datos2010/src.datos.treeB/FreeBlockList.h
+++ Datos2010/src.datos.treeB/FreeBlockList.h
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -21,10 +22,28 @@ public:
 	FreeBlockList(int blocksize);
 	virtual ~FreeBlockList();
 
+	int open(string name, ios_base::openmode mode);
+	int create(string name, ios_base::openmode mode);
+	int close();
+	void add(int blockId);
+	int get();
+	int writeSimpleBlock(int addr);
+	int readSimpleBlock(int addr);
+
 
 private:
-	FixedFieldBuffer buffer;
+	FixedFieldBuffer buffer;			//Buffer que contiene el bloque doble inicial
+	FixedFieldBuffer simpleBuffer;		//Buffer de bloques simples
 	BufferFile bTreeFile;
+	int blockSize;
+	vector<int> freeBlocks;		//vector con todos los bloques libres
+	unsigned int maxFields;		//Maximo numero de bloques por bloque del archivo de libres
+	int nBlocks;				//Cantidad de bloques libres
+	bool overflow,underflow;
+
+	int pack();
+	int unpack();
+
 
 };
 
