c4f36df39c339099b1eda7d51eb2f7ab031cd0f1
santiagodonikian <santiagodonikian@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-04-28 02:47:29 +0000

    mains viejos pasar a clase Test.cpp

--- Datos2010/hashExtensible/MainHash.cpp
+++ Datos2010/hashExtensible/MainHash.cpp
@@ -112,7 +112,7 @@ static void parse_cmdline(int argc, char * const argv[]){
 
 }
 
-int main(int argc, char * const argv[]){
+int maingfas(int argc, char * const argv[]){
 	parse_cmdline(argc,argv);
 	return 0;
 }
