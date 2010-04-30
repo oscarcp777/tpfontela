5d74f6cf1efe22b1c5ddd5c183fcd68070b7dc75
lomasfragildelalocura <lomasfragildelalocura@a1477896-89e5-11dd-84d8-5ff37064ad4b>
2010-04-30 18:11:22 +0000

    test del logger. bug arreglado del close de btree.

--- Datos2010/src.datos.test/Test.cpp
+++ Datos2010/src.datos.test/Test.cpp
@@ -8,6 +8,7 @@
 #include "TestIOFile.h"
 #include "TestFreeBlockController.h"
 #include "TestBTree.h"
+#include "TestLogger.h"
 #include "../src.datos.utils/Define.h"
 
 int main(){
@@ -15,6 +16,7 @@ int main(){
 	TestIOFile* testIOFile = new TestIOFile();
 	TestFreeBlockController* testFreeBlockController = new TestFreeBlockController();
 	TestBTree* testBTree = new TestBTree();
+	TestLogger* testLogger = new TestLogger();
 
 
 //	testIOFile->runBinaryFileTest2();
@@ -22,10 +24,12 @@ int main(){
 //	testFreeBlockController->runGeneralTest();
 //	testFreeBlockController->runAddTest();
 //	testFreeBlockController->runGetTest();
-	testBTree->runTestInsert("files/testbt.dat", 128);
-//	testBTree->runTestSecuenceSet("files/testbt.dat",128);
+//	testBTree->runTestInsert("files/testbt.dat", 128);
+	testBTree->runTestSecuenceSet("files/testbt.dat",128);
 //	testBTree->runTestRemove("files/testbt.dat",128);
+//	testLogger->runTestLogger();
 
+	delete testLogger;
 	delete testIOFile;
 	delete testFreeBlockController;
 	delete testBTree;
